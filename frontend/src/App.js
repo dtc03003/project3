18484cb0ff326959fe0bd6d21488227fe5927d57
김동현 <dtc03003@naver.com>
2022-05-19 10:16:01 +0900

    [FE-김동현][develop] feat: 로딩구현
    Former-commit-id: 810543f91731dac3d589623449fd7c45bcc2f6ef

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 
 const unityContext = new UnityContext({
@@ -9,17 +9,45 @@ const unityContext = new UnityContext({
   codeUrl: "Build/WebGL.wasm",
 })
 
-function App() {
+export default function App() {
+  const [LoadingStyle, setLoadingStyle] = useState({ display: 'block' });
+  const [UnityStyle, setUnityStyle] = useState({ display: 'none' })
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setLoadingStyle({ display: 'none' })
+      setUnityStyle({ display: 'block' })
+    }, 20000);
+    return () => { clearTimeout(timer) }
+  }, []);
+
   return (
-    <div >
-      <Unity style={{
-        width: '1600px',
-        justifySelf: 'center',
-        alignSelf: 'center'
-      }}
+    <div>
+      <div style={LoadingStyle}><LoadingComponent/></div>
+      <div style={UnityStyle}> <UnityComponent/></div>
+    </div>
+  );
+};
+
+function UnityComponent() {
+  return (
+    <div>
+      <Unity
+        style={{
+          width: '1600px',
+          height: '800px',
+          justifySelf: 'center',
+          alignSelf: 'center'
+        }}
         unityContext={unityContext} />
     </div>
   );
-}
+};
 
-export default App;
+function LoadingComponent() {
+  return (
+    <div>
+      <img src="https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/1Bie/image/8qbDlFf1RDkPimUoyooxeNSqIic.jpg"></img>
+    </div>
+  );
+};
